555af05e2fe7878c301acfd595e59823bb3402bd
Matthew Tompsett <matthewbpt@gmail.com>
2015-01-15 18:53:38 +0000

    added support for custom mapping

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #[macro_export]
 macro_rules! query_map {
-($conn:ident, $query:expr, $params:expr, $T:ident, $($member:ident),* ) => {
+	($conn:ident, $query:expr, $params:expr, $T:ident{ $($member:ident),* }) => {
 		{		
 			let stmt = $conn.prepare($query)
 					.unwrap();
@@ -10,6 +10,19 @@ macro_rules! query_map {
 						$( $member: row.get(stringify!($member)) ),*
 					});
 			result.collect::<Vec<$T>>()			
+		}
+	};
+	
+	($conn:ident, $query:expr, $params:expr, $T:ident{ $($member:ident => $key:expr),* }) => {
+		{
+			let stmt = $conn.prepare($query)
+					.unwrap();
+				
+			let result = stmt.query($params).unwrap()
+				.map(|row:postgres::Row| $T {
+						$( $member: row.get($key) ),*
+					});
+			result.collect::<Vec<$T>>()	
 		}
 	}
 }
