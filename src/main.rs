093938e2e52204fbb2d2622ea346ebabdd1c5b0f
Matthew Tompsett <matthewbpt@gmail.com>
2015-01-16 22:30:13 +0000

    fix formatting in main

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,12 @@ use postgres::{Connection, SslMode};
 
 struct Person {
     id: i32,
-    name: String,
-    time_created: Timespec,
-    data: Option<Vec<u8>>
+    name: String
 }
 
 impl fmt::Show for Person {
-	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Person {{ id: {}, name: {}, time_created: {:?}, data: {:?} }}", self.id, self.name, self.time_created, self.data)
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Person {{ id: {}, name: {} }}", self.id, self.name)
     }
 }
 
@@ -48,29 +46,43 @@ fn main() {
                     VALUES ($1, $2, $3)",
                  &[&you.name, &you.time_created, &you.data]).unwrap();
 */
-	let conn_uri = "postgres://postgres:password@localhost:5555";
-	
-	let conn = Connection::connect(conn_uri, &SslMode::None)
-            .unwrap();
+    let conn_uri = "postgres://postgres:password@localhost:5555";
+    let conn = match Connection::connect(conn_uri, &SslMode::None) { Ok(c) => c, Err(m) => return };
 
-	let prepared_sql_string = "SELECT id, name, time_created, data FROM person";
+    // prepare a string
+    let prepared_sql_string = "SELECT id, name FROM person WHERE id = $1";
 
-	for person in query_map!(conn, prepared_sql_string, &[],
-		Person { id, name, time_created, data } // The struct name and fields within the struct
-		).unwrap().iter() {
-		println!("{:?}", person)
-	}
-	
-	let result = query_map!(conn, prepared_sql_string, &[], 
-		Person { id => "id", name => "name", time_created => "time_created", data => "data" }
-		);
-	
-	match result {
-		Ok(r) => {
-			for person in r.iter() {
-				println!("{:?}", person);
-			}
-		},
-		Err(m) => println!("{:?}", m),
-	}	
+    // use the query_map! macro to execute the query and map each row to a Person
+    // with columns "id" mapped to id and "name" mapped to name, it takes the
+    // names of the fields specified and maps the column with the same name
+    let result = query_map!(conn, prepared_sql_string, &[&2],
+        Person { id, name }
+    );
+
+    // iterate through resultset and print each row
+    match result {
+        Ok(r) => {
+            for person in r.iter() {
+                println!("id: {}, name: {}", person.id, person.name);
+            }
+        },
+        Err(m) => println!("{:?}", m),
+    };
+
+    let another_prepared_sql_string = "SELECT person_id, full_name FROM person WHERE id = $1";
+
+    // same as above, but instead we provide a custom mapping if the columns
+    // in the query don't match fields in the struct
+    let result2 = query_map!(conn, another_prepared_sql_string, &[&1], 
+        Person { id => "person_id", name => "full_name" }
+    );
+
+    match result2 {
+        Ok(r) => {
+            for person in r.iter() {
+                println!("id: {}, name: {}", person.id, person.name);
+            }
+        },
+        Err(m) => println!("{:?}", m),
+    }; 
 }
